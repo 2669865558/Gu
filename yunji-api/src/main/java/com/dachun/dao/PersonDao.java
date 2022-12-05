0c0e59b1f13a054b80e2c2a9d8816429dba1f4c0
Akihi <1171401949@qq.com>
2022-12-05 20:09:03 +0800

    Initial commit

---
+++ yunji-api/src/main/java/com/dachun/dao/PersonDao.java
@@ -0,0 +1,42 @@
+package com.dachun.dao;
+
+
+import com.dachun.entity.Person;
+import com.dachun.entity.PersonQuery;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface PersonDao {
+
+    @Insert(" insert into a_person (id,name,phone,address,venueId) values " +
+            " (#{id},#{name},#{phone},#{address},#{venueId}) ")
+    int insert(Person person);
+
+    @Update(" update a_person set name = #{name}, phone = #{phone}, address = #{address}," +
+            " venueId = #{venueId} " +
+            " where id = #{id}")
+    int update(Person venue);
+
+    @Select("<script> select  *  from  a_person" +
+            " where 1 = 1 " +
+            " <if test = 'id != null'> " +
+            " and id = #{id} " +
+            "</if>" +
+            " <if test = 'name != null'> " +
+            " and name like concat ('%',trim(#{name}),'%') " +
+            "</if>" +
+            " <if test = 'phone != null'> " +
+            " and phone = #{phone} " +
+            "</if>" +
+            " <if test = 'venueId != null'> " +
+            " and venueId = #{venueId} " +
+            "</if>" +
+            "</script>")
+    List<Person> select(PersonQuery query);
+
+    @Delete(" delete from a_person where id = #{id}")
+    int delete(String id);
+
+}
